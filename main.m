7d504b176facaa70530d359a75de028f93cfae82
Hilding Wollbo <Hilding@n184-p182.eduroam.kth.se>
2019-02-27 14:26:55 +0100

    LMS

--- main.m
+++ main.m
@@ -6,10 +6,18 @@
 %%
 
 % LMS
-   
+N = 128;
+muu = 0.00001;
+
+[theta, xhat, delta] = LMS(y, N, muu);
+% for i = 1:length(y)
+%     [xhat, theta] = LMS(y,N,muu);
+% end
+
+
  % temp
 
-%soundsc(y)
+soundsc(xhat)
 
 
 %%
